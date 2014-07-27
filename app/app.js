1eb9200d0ce873833a6cd3d663bd06cfadd37a81
mudzereli <mudz@charter.net>
2014-07-27 16:14:33 -0500

    1.0.3
    framework setup

--- app/app.js
+++ app/app.js
@@ -4,8 +4,35 @@
 * Description
 *   dayz addon pack module
 */
-angular.module('DZAP', [])
-.controller('DZAPController', ['', function(){
-    
+angular.module('DZAP', ['ngRoute'])
+.config(['$routeProvider',function($routeProvider) {
+    $routeProvider
+    .when("/home", {
+        templateUrl: "app/home/home.html"
+    })
+    .when("/about", {
+        templateUrl: "app/about/about.html"
+    })
+    .when("/install", {
+        templateUrl: "app/install/install.html"
+    })
+    .when("/config", {
+        templateUrl: "app/config/config.html"
+    })
+    .when("/contact", {
+        templateUrl: "app/contact/contact.html"
+    })
+    .otherwise({
+        redirectTo: '/home'
+    });
+}])
+.controller('DZAPController', ['$scope', function($scope){
+    $scope.addon = {
+        name: "DayZ Epoch Addon Pack",
+        version: [
+            {name: "1.0.0", desc:"first release"},
+            {name: "1.0.0", desc:"first release"}
+        ]
+    };
 }])
 ;
